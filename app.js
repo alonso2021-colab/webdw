24287bc497a7d1530c10259e40bc6414062527ae
alonso2021-colab <geldresalonso8@gmail.com>
2026-09-09 09:32:33 -0300

    post

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Paso 1. 
 require('dotenv').config();
+const { name } = require('ejs');
 const express = require('express')
 
 const path = require('path');
@@ -13,6 +14,10 @@ const port = process.env.port;
 
 // paso 4. 
 app.set('view engine', 'ejs')
+
+// MIDDLEWARES
+// Permite leer info enviada desde el formulario HTML
+app.use(express.urlencoded({extended: true}));
 
 // paso 5. archivos estaticos
 app.use(express.static(path.join(__dirname, 'public')));
@@ -32,7 +37,31 @@ app.get('/nosotros',(req, res) => {
 })
 
 app.get('/contacto',(req, res) => {
-  res.render('contacto' )
+  res.render('contacto', {
+    mensajeExito: null
+  } )
+})
+
+app.post('/contacto', (req, res) => {
+  const {
+    nombre,
+    correo,
+    Asunto, 
+    mensaje
+  } = req.body
+
+  console.log('-----------------')
+  console.log('Nuevo mensaje')
+  console.log('-----------------')
+  console.log('nombre: ', nombre)
+  console.log('correo: ', correo)
+  console.log('asunto: ', Asunto)
+  console.log('Mensaje: ', mensaje)
+
+  res.render('contacto', {
+    mensajeExito: 'Tu informacion se envio correctamente'
+  })
+
 })
 
 
